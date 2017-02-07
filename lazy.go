96bf89823aa8d0b2edee6f20d7bdac7803e972d6
jame2981 <jame2981@gmail.com>
2017-02-07 14:35:49 +0800

    add lazy interface

--- lazy.go
+++ lazy.go
@@ -1,43 +1,5 @@
 package rhombus
 
-type (
-	Lazy interface {
-		Resolve(c *Context)
-	}
-
-	LazyString struct {
-		Key string
-		Val string
-	}
-
-	LazyStrings struct {
-		Key string
-		Val []string
-	}
-
-	LazyInt struct {
-		Key string
-		Val int
-	}
-
-	LazyInts struct {
-		Key string
-		Val []int
-	}
-)
-
-func (l *LazyString) Resolve(c *Context) {
-	l.Val = c.Get(l.Key).(string)
-}
-
-func (l *LazyStrings) Resolve(c *Context) {
-	l.Val = c.Get(l.Key).([]string)
-}
-
-func (l *LazyInt) Resolve(c *Context) {
-	l.Val = c.Get(l.Key).(int)
-}
-
-func (l *LazyInts) Resolve(c *Context) {
-	l.Val = c.Get(l.Key).([]int)
+type Lazy interface {
+	Resolve(c *Context)
 }
